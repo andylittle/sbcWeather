42f5e208ea3ec78bde2c0d3f7e8dbf6fde5c1881
Little. Andrew. H <andy.little@consolidated.com>
2016-05-27 13:45:24 -0700

    correct file

--- inprogress.ino
+++ inprogress.ino
@@ -25,9 +25,13 @@
 // Example sketch for DHT22 humidity - temperature sensor
 // Written by cactus.io, with thanks to Adafruit for bits of their library. public domain
 
-#include "cactus_io_DHT22.h"
+
+#include "cactus_io_DHT22.h" //TempSensor lib
+#include "BMP280.h" //BMP280 libs
+#include "Wire.h"   //BMP280 libs
 
 #define DHT22_PIN 2     // what pin on the arduino is the DHT22 data line connected to
+#define P0 1013.25 //BMP280 ? 
 
 // For details on how to hookup the DHT22 sensor to the Arduino then checkout this page
 // http://cactus.io/hookups/sensors/temperature-humidity/dht22/hookup-arduino-to-dht22-temp-humidity-sensor
@@ -41,51 +45,82 @@ DHT22 dht(DHT22_PIN);
 // cycles transitioning between bits on the data and cloines. For the
 // Arduino boards that run at 84MHz the value of 30 should be about right.
 
+BMP280 bmp;
 
-
-void setup() {
+void setup() 
+{
   Serial.begin(9600); 
-//  Serial.println("DHT22 Humidity - Temperature Sensor");
-//  Serial.println("RH\t\tTemp (C)\tTemp (F)\tHeat Index (C)\t Heat Index (F)");
 
   dht.begin();
+  bmp.begin();
+ // if(!bmp.begin()){
+   // Serial.println("BMP init failed!");
+  //  while(1);
+//  }
+//  else Serial.println("BMP init success!");
+  
+  bmp.setOversampling(4);  
 }
 
-void loop() {
+void loop() 
+{
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   dht.readHumidity();
   dht.readTemperature();
+  
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(dht.humidity) || isnan(dht.temperature_C)) {
     Serial.println("DHT sensor read failure!");
-    return;
+    //return;
+    
+    
+  double T,P;
+  char result = bmp.startMeasurment();
+ 
+  if(result!=0){
+    delay(result);
+    result = bmp.getTemperatureAndPressure(T,P);
+    //Serial.println(T,2);
+    //Serial.println(P,2);
+      if(result!=0)
+      {
+        double A = bmp.altitude(P,P0);
+    //    int ALT = (A,2); 
+        Serial.println(T,2);
+        Serial.println(P,2);   
+      }
+      else {
+        Serial.println("Error.");
+      }   
+    // return; 
   }
-  
+ } 
   
   int outTEM = (dht.temperature_F * 100);
   int inHUM = (dht.humidity * 100);
-  //dht.temperature_F = int(dht.temperature_F);
-  //dht.humidity = round(dht.humidity);
+  //int inTEM = (100);
+  //int inPRE = 100;
+  
   Serial.print("!!");
-  Serial.print("0000"); 
-  Serial.print("00");
-  Serial.print("BE"); 
-  Serial.print(outTEM,HEX); 
-  Serial.print("0000");
-  Serial.print("2770"); 
-  Serial.print("0000"); 
-  Serial.print(inHUM,HEX); 
+  Serial.print("0000"); //WindSpeed
+  Serial.print("00");  //Wind direction calibration
+  Serial.print("BE");   // Wind direction 
+  Serial.print(outTEM,HEX); //outTEM
+  Serial.print("0000");  //longterm rain
+  //Serial.print(inPRE,HEX); //pressure
+  //Serial.print(inTEM,HEX); //inTemp
+  Serial.print(inHUM,HEX); //inHum
   Serial.print("023A");
   Serial.print("0025");   
   Serial.print("0058");
   Serial.print("0000"); 
   Serial.println("0000");  
-  //Serial.println(dht.temperature_F);
-  //Serial.println(dht.humidity);  
+  //Serial.println(inTEM);
+  //Serial.println(inPRE);  
 
-  //Serial.println(dht.computeHeatIndex_F());
+  
 
   // Wait a few seconds between measurements. The DHT22 should not be read at a higher frequency of
   // about once every 2 seconds. So we add a 3 second delay to cover this.
